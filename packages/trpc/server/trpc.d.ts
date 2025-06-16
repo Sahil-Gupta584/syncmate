7faaeaa2b2044724da47328136b403df59f15c4c
Sahil Gupta <guptas3067@gmail.com>
2025-06-16 15:21:30 +0530

    backend build

--- packages/trpc/server/trpc.d.ts
+++ packages/trpc/server/trpc.d.ts
@@ -1,25 +1,8 @@
-export declare const trpcProcedure: import("@trpc/server/unstable-core-do-not-import").ProcedureBuilder<
-  object,
-  object,
-  object,
-  typeof import("@trpc/server/unstable-core-do-not-import").unsetMarker,
-  typeof import("@trpc/server/unstable-core-do-not-import").unsetMarker,
-  typeof import("@trpc/server/unstable-core-do-not-import").unsetMarker,
-  typeof import("@trpc/server/unstable-core-do-not-import").unsetMarker,
-  false
->;
-export declare const trpcRouter: <
-  TInput extends
-    import("@trpc/server/unstable-core-do-not-import").CreateRouterOptions,
->(
-  input: TInput,
-) => import("@trpc/server/unstable-core-do-not-import").BuiltRouter<
-  {
+export declare const trpcProcedure: import("@trpc/server/unstable-core-do-not-import").ProcedureBuilder<object, object, object, typeof import("@trpc/server/unstable-core-do-not-import").unsetMarker, typeof import("@trpc/server/unstable-core-do-not-import").unsetMarker, typeof import("@trpc/server/unstable-core-do-not-import").unsetMarker, typeof import("@trpc/server/unstable-core-do-not-import").unsetMarker, false>;
+export declare const trpcRouter: <TInput extends import("@trpc/server/unstable-core-do-not-import").CreateRouterOptions>(input: TInput) => import("@trpc/server/unstable-core-do-not-import").BuiltRouter<{
     ctx: object;
     meta: object;
     errorShape: import("@trpc/server/unstable-core-do-not-import").DefaultErrorShape;
     transformer: false;
-  },
-  import("@trpc/server/unstable-core-do-not-import").DecorateCreateRouterOptions<TInput>
->;
+}, import("@trpc/server/unstable-core-do-not-import").DecorateCreateRouterOptions<TInput>>;
 //# sourceMappingURL=trpc.d.ts.map
