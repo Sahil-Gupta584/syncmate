019e50f6fbfda614eded0fb9ca13863f38ce145a
Sahil Gupta <guptas3067@gmail.com>
2025-06-25 23:14:47 +0530

    d

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useEffect } from "react";
 import Features from "./components/features";
 import Footer from "./components/footer";
 import Header from "./components/header";
@@ -8,15 +7,15 @@ import Pricing from "./components/pricing";
 import WhyUs from "./components/whyUs";
 
 export default function Home() {
-  useEffect(() => {
-    if (window.location.hash) {
-      const id = window.location.hash.replace("#", "");
-      const el = document.getElementById(id);
-      if (el) {
-        el.scrollIntoView({ behavior: "smooth" });
-      }
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (window.location.hash) {
+  //     const id = window.location.hash.replace("#", "");
+  //     const el = document.getElementById(id);
+  //     if (el) {
+  //       el.scrollIntoView({ behavior: "smooth" });
+  //     }
+  //   }
+  // }, []);
 
   return (
     <>
