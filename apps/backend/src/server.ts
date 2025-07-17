df2d50c02e96dce108fb6047979cb696b8926490
Sahil Gupta <guptas3067@gmail.com>
2025-07-17 18:04:28 +0530

    12

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,5 +1,4 @@
 import { prisma } from "@repo/db";
-
 import { appRouter } from "@repo/trpc";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import { toNodeHandler } from "better-auth/node";
@@ -77,7 +76,7 @@ app.all("/creator/api/auth/*any", toNodeHandler(creatorAuth));
 app.all("/editor/api/auth/*any", toNodeHandler(editorAuth));
 
 app.get("/", (req, res) => {
-  res.send("Backend is working");
+  res.send("Backend is working....");
 });
 
 app.post("/webhook", express.raw({ type: "*/*" }), async (req, res) => {
